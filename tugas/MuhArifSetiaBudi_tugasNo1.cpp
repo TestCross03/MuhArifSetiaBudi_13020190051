797378eb6b953cd4b55e839f67904a276f359e0d
TestCross03 <61418572+TestCross03@users.noreply.github.com>
2020-03-08 15:06:14 +0800

    Add files via upload

---
+++ tugas/MuhArifSetiaBudi_tugasNo1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int penjumlahan, a, b;
+	int J[b];
+	
+	cout << "jumlah bilangan = ";
+	cin >> b;
+	
+	for (a=0;a<b;a++){
+		cin >> J[a];
+	}
+	for (a=0;a<b;a++){
+		penjumlahan = penjumlahan + J[a];
+}
+	cout << "Hasil Penjumlahan = "<<penjumlahan;
+}
